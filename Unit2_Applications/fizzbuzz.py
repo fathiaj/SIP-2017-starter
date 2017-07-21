9859eea7eca9dda80256560db62c90a198c55362
fathiaj <fathiajirde@gmail.com>
2017-07-21 14:37:40 -0700

    Create fizzbuzz.py

---
+++ Unit2_Applications/fizzbuzz.py
@@ -0,0 +1,14 @@
+print ("how many numbers should the program go up to?")
+user_input=input()
+x=int (user_input)
+loop = 0
+for hops in range(x):
+    loop=loop+1
+    if loop % 3 == 0 and loop%5 == 0:
+          print ("fizzbuzz")
+    elif loop % 3 == 0:
+          print ("fizz")
+    elif loop % 5 == 0:
+          print ("buzz")
+    else:
+        print(loop)
